35aadc518c405512ccb01aa97ac27496f683c2b3
Nikita G <jobnikitka152rus@gmail.com>
2023-08-29 17:03:53 +0300

    edit PayPage

--- src/test/java/ru/netology/pages/PayPage.java
+++ src/test/java/ru/netology/pages/PayPage.java
@@ -1,6 +1,10 @@
 package ru.netology.pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selectors.byText;
@@ -21,4 +25,32 @@ public class PayPage {
     private final SelenideElement incorrectFormat = $(byText("Неверный формат"));
     private final SelenideElement successNotification = $(byText("Операция одобрена Банком."));
     private final SelenideElement errorNotification = $(byText("Ошибка! Банк отказал в проведении операции."));
+
+    public void fillCardData(DataHelper.CardData cardData) {
+        cardNumberField.setValue(cardData.getNumber());
+        monthField.setValue(cardData.getMonth());
+        yearField.setValue(cardData.getYear());
+        ownerField.setValue(cardData.getOwner());
+        cvcField.setValue(cardData.getCvc());
+        button.click();
+    }
+    public void verifyEmptyField() {
+        emptyField.shouldBe(Condition.visible);
+    }
+    public void verifyCardValidity() {
+        cardValidity.shouldBe(Condition.visible);
+    }
+    public void verifyInvalidCardExpirationDate() {
+        invalidСardExpirationDate.shouldBe(Condition.visible);
+    }
+    public void verifyIncorrectFormat() {
+        incorrectFormat.shouldBe(Condition.visible);
+    }
+    public void verifySuccessNotification() {
+        successNotification.shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    public void verifyErrorNotification() {
+        errorNotification.shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
 }
