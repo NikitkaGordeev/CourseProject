9c44d85af6860169e7060d95cfa176fb5b662d04
Nikita G <jobnikitka152rus@gmail.com>
2023-09-03 12:08:34 +0300

    edit MainPage.java

--- src/test/java/ru/netology/pages/MainPage.java
+++ src/test/java/ru/netology/pages/MainPage.java
@@ -8,18 +8,15 @@ import static com.codeborne.selenide.Selenide.$$;
 public class MainPage {
     private final SelenideElement buyButton = $$("button").find(Condition.exactText("Купить"));
     private final SelenideElement buyCreditButton = $$("button").find((Condition.exactText("Купить в кредит")));
-    private final SelenideElement payment = $$("h3.heading").find(Condition.exactText("Оплата по карте"));
-    private final SelenideElement credit = $$("h3.heading").find(Condition.exactText("Кредит по данным карты"));
+
 
     public PayPage clickByuButton() {
         buyButton.click();
-        payment.shouldBe(Condition.visible);
         return new PayPage();
     }
 
     public PayPage clickCreditButton() {
         buyCreditButton.click();
-        credit.shouldBe(Condition.visible);
         return new PayPage();
     }
 }
